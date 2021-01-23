61ebbbe0f4c8a53a530e4f9f6c5df5b742673d8b
consolekakao <console@kakao.com>
2021-01-24 06:55:25 +0900

    fix err

--- test.js
+++ test.js
@@ -335,6 +335,11 @@ else if(message.content.startsWith(`!커넥트`) && (message.channel.guild.owner
         `insert into connectbotlist (servername,channelname,serverid,channelid) values ('${message.channel.guild.name}','${message.channel.name}','${message.guild.id}','${message.channel.id}')`
       );
       message.channel.send("연합채널들과 연결되었어요!")
+      connection.query(
+        `insert into BotLog (servername,channelname,usernick,time,usecommand,status,errormessage) values 
+        ('${message.channel.guild.name}','${message.channel.name}','${message.author.username +' #' +message.author.discriminator}',
+        '${insertTime}','${message.content}','OK','')`
+      );
       return;
 }
 
@@ -345,6 +350,11 @@ else if(message.content.startsWith(`!디스커넥트`) && (message.channel.guild
         `delete from connectbotlist where serverid = '${message.guild.id}' and channelid = '${message.channel.id}'`
       );
       message.channel.send("연합채널들과 연결이 중단되었어요!")
+      connection.query(
+        `insert into BotLog (servername,channelname,usernick,time,usecommand,status,errormessage) values 
+        ('${message.channel.guild.name}','${message.channel.name}','${message.author.username +' #' +message.author.discriminator}',
+        '${insertTime}','${message.content}','OK','')`
+      );
       return;
 }
 
@@ -467,6 +477,11 @@ function teamSplit(message){
   if(isNaN(Number(args[1])) || Number(args[1]) == 0)
   {
     message.channel.send("사용법이 이상해요. \n!팀배정 <팀별인원수> 멤버A 멤버B 멤버C\nex) !팀배정 3 팀원A 팀원B 팀원C 팀원D")
+    connection.query(
+      `insert into BotLog (servername,channelname,usernick,time,usecommand,status,errormessage) values 
+      ('${message.channel.guild.name}','${message.channel.name}','${message.author.username +' #' +message.author.discriminator}',
+      '${insertTime}','${message.content}','-','SYNTAX ERR')`
+    );
     return;
   }
   let data = [];
@@ -519,6 +534,11 @@ re.addFields(
 )
 }
 message.reply('',re)
+connection.query(
+  `insert into BotLog (servername,channelname,usernick,time,usecommand,status,errormessage) values 
+  ('${message.channel.guild.name}','${message.channel.name}','${message.author.username +' #' +message.author.discriminator}',
+  '${insertTime}','${message.content}','OK','')`
+);
 }
 
 
