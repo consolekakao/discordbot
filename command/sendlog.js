e2bd0751499cfbd034f2f018321878e178454a3e
consolekakao <console@kakao.com>
2021-02-14 23:36:25 +0900

    Update path

--- command/sendlog.js
+++ command/sendlog.js
@@ -6,15 +6,15 @@ async function sendLog(message,number,insertTime){
         let time = date.getMonth()+1
         if(number == "1")
         {
-           await message.reply({files:['/home/consolekakao/newerr.txt'] });
-           await fs.unlinkSync('/home/consolekakao/newerr.txt')
-           await fs.writeFileSync('/home/consolekakao/newerr.txt',`${insertTime}`,function(err){if(err === null)console.log("에러로그 정상출력")})
+           await message.reply({files:['/home/remaylin/newerr.txt'] });
+           await fs.unlinkSync('/home/remaylin/newerr.txt')
+           await fs.writeFileSync('/home/remaylin/newerr.txt',`${insertTime}`,function(err){if(err === null)console.log("에러로그 정상출력")})
         }
         if(number == "2")
         {
-           await message.reply({files:['/home/consolekakao/newoutput.txt'] });
-           await fs.unlinkSync('/home/consolekakao/newoutput.txt')
-           await fs.writeFileSync('/home/consolekakao/newoutput.txt',`${insertTime}`,function(err){if(err === null)console.log("봇로그 정상출력")})
+           await message.reply({files:['/home/remaylin/newoutput.txt'] });
+           await fs.unlinkSync('/home/remaylin/newoutput.txt')
+           await fs.writeFileSync('/home/remaylin/newoutput.txt',`${insertTime}`,function(err){if(err === null)console.log("봇로그 정상출력")})
         }
    
   
