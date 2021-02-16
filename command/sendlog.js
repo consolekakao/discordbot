94b7118abb16a6de9c35154a40601376aa23c4d8
consolekakao <console@kakao.com>
2021-02-16 23:57:33 +0900

    Update insert time

--- command/sendlog.js
+++ command/sendlog.js
@@ -8,13 +8,13 @@ async function sendLog(message,number,insertTime){
         {
            await message.reply({files:['/home/remaylin/newerr.txt'] });
            await fs.unlinkSync('/home/remaylin/newerr.txt')
-           await fs.writeFileSync('/home/remaylin/newerr.txt',`${insertTime}`,function(err){if(err === null)console.log("에러로그 정상출력")})
+           await fs.writeFileSync('/home/remaylin/newerr.txt',`${insertTime}`,function(err){if(err === null)console.log(`에러로그 정상출력 ${insertTime}`)})
         }
         if(number == "2")
         {
            await message.reply({files:['/home/remaylin/newoutput.txt'] });
            await fs.unlinkSync('/home/remaylin/newoutput.txt')
-           await fs.writeFileSync('/home/remaylin/newoutput.txt',`${insertTime}`,function(err){if(err === null)console.log("봇로그 정상출력")})
+           await fs.writeFileSync('/home/remaylin/newoutput.txt',`${insertTime}`,function(err){if(err === null)console.log(`봇로그 정상출력 ${insertTime}`)})
         }
    
   
